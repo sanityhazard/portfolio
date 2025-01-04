93d6997084db4f66dbfd7116457f32bfc87bdba2
sanityhazard <ego.ivlev@gmail.com>
2025-01-04 19:13:29 +0200

    version 1

---
+++ pages/index.vue
@@ -0,0 +1,110 @@
+<style>
+@import url("~/public/styles.scss");
+</style>
+
+<template>
+    <div class="roboto-flex center">
+        <div class="content">
+            <div class="info">
+                <div class="main">
+                    <h1>ivlev egor</h1>
+                    <p>an 18 year old with 25 years of experience</p>
+                </div>
+                <div class="secondary">
+                    <p>I’ve had to put active development on pause due to personal and health issues. Instead of focusing on long-term pet projects, I have switched to freelance tasks and studying algorithms & data structures in my spare time.</p>
+                    <p>Not all projects are useful or serve any purpose at all. Some were made just for fun. I simply like them.</p>
+                    <p>Not all projects are displayed here. I have spent almost the entirety of 2023-2024 working on a team project and some small freelance projects I am unable display here. Message me and I’ll arrange a showcase if I can.</p>
+                </div>
+            </div>
+            <div class="project">
+                <div class="plaques">
+                    <p class="plaque-main">present</p>
+                    <p class="plaque">Nuxt (Vue)</p>
+                    <p class="plaque">Pinia</p>
+                    <p class="plaque">FastAPI</p>
+                    <p class="plaque">Kinde</p>
+                    <p class="plaque">i18n</p>
+                </div>
+                <div class="info">
+                    <h2>crohntrack.</h2>
+                    <p>Easily monitor your Crohn’s disease flow with crohntrack - an easy to use, user-oriented webapp. <span>(intended for use on mobile)</span></p>
+                    <a href="https://crohntrack.ivlev.xyz">take a look (work in progress)</a>
+                </div>
+            </div>
+            <div class="project">
+                <div class="plaques">
+                    <p class="plaque-main">feb 2024</p>
+                    <p class="plaque">aiogram</p>
+                </div>
+                <div class="info">
+                    <h2>valentinka</h2>
+                    <p>A telegram-bot that allows people to send anonymous messages to each other and reply to them. Was made specifically for Valentine’s Day.</p>
+                    <a href="https://t.me/valentiiinkabot">take a look</a>
+                </div>
+            </div>
+            <div class="project">
+                <div class="plaques">
+                    <p class="plaque-main">jan 2024</p>
+                    <p class="plaque">React</p>
+                    <p class="plaque">Vite</p>
+                    <p class="plaque">React Draggable</p>
+                    <p class="plaque">Django</p>
+                </div>
+                <div class="info">
+                    <h2>casecalc</h2>
+                    <p>A case* simulator website. Each case can be configured, simulated, exported or saved for later use. Has to be self-hosted for everything to be saved.</p>
+                    <a href="https://casecalc.ivlev.xyz">take a look (does not allow persistence for security reasons)</a>
+                    <p><span>*case - in video games, like a lottery but with in-game item-based prizes</span></p>
+                </div>
+            </div>
+            <div class="project">
+                <div class="plaques">
+                    <p class="plaque-main">jan 2024</p>
+                    <p class="plaque-hollow">joke project</p>
+                    <p class="plaque">aiogram</p>
+                </div>
+                <div class="info">
+                    <h2>tradertypebot</h2>
+                    <p>An inline telegram bot that randomly selects a number from 1 to 100 and based on it, determines what category of crypto/stock market traders a person belongs in. Was made as a joke. Should not be mistaken as financial advice. Although the responses may be offensive, I do not intend to offend anyone. Has Russian language support only! <span>Since it's an inline bot, its username should be entered in a message field. Then, a button will appear</span></p>
+                    <a href="https://t.me/trader_type_bot">take a look</a>
+                </div>
+            </div>
+            <div class="project">
+                <div class="plaques">
+                    <p class="plaque-main">oct 2022</p>
+                    <p class="plaque">React</p>
+                    <p class="plaque">Framer Motion</p>
+                    <p class="plaque">Django</p>
+                </div>
+                <div class="info">
+                    <h2>taste</h2>
+                    <p>A website that makes searching for a movie more fun, since it allows its users to swipe left or right depending on whether they would want to watch a movie or not - like Tinder. Has an account system that saves liked movies.</p>
+                    <a href="https://taste.ivlev.xyz">take a look</a>
+                </div>
+            </div>
+            <div class="project">
+                <div class="plaques">
+                    <p class="plaque-main">jul 2022</p>
+                    <p class="plaque">Minecraft Forge</p>
+                </div>
+                <div class="info">
+                    <h2>wanderer</h2>
+                    <p>A minecraft mod that adds special runes, each with unique abilities into the game. Also adds structures in which these runes can be found.</p>
+                    <a href="https://legacy.curseforge.com/minecraft/mc-mods/the-wanderer-runes">take a look</a>
+                </div>
+            </div>
+            <div class="project">
+                <div class="plaques">
+                    <p class="plaque-main">oct 2021</p>
+                    <p class="plaque">discord.py</p>
+                </div>
+                <div class="info">
+                    <h2>dst crafts</h2>
+                    <p>A discord bot that allows people to look for all Don’t Starve Together (videogame) recipes (not including mods) without leaving discord.</p>
+                    <a href="https://github.com/sanityhazard/DST_crafts">take a look (github only)</a>
+                </div>
+            </div>
+            <div class="copyright">© 2025 Ivlev Egor</div>
+        </div>
+    </div>
+</template>
