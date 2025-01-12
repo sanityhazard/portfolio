40b2b3e8351721f468b626b64eff96917f3294c0
sanityhazard <ego.ivlev@gmail.com>
2025-01-12 16:13:12 +0200

    changed meta

--- app.vue
+++ app.vue
@@ -4,3 +4,13 @@
     <NuxtRouteAnnouncer />
   </div>
 </template>
+
+
+<script>
+useHead({
+  title: "Ivlev's Portfolio",
+  meta: [
+    { name: 'description', content: 'Ivlev Egor\'s projects' }
+  ],
+})
+</script>
